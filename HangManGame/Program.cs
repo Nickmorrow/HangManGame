cebd41ace8f7a33ce052f033ef1813047ae54651
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2021-12-01 20:01:30 -0500

    Add project files.

---
+++ HangManGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HangManGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool playAgain = true;
+            string yesString = "y";
+
+            while (playAgain)
+            {
+
+
+
+
+
+
+                Console.WriteLine("Would you like to play again y/n?");
+                string userAnswer = Console.ReadLine();
+             
+                playAgain = userAnswer == yesString; // short option playAgain
+            }
+        }
+    }
+}
