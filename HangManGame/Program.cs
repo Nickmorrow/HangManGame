1a827d939b4bb44e4d39310d61e4856be668170c
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2021-12-05 12:29:23 -0500

    initial commit / main game loop

--- HangManGame/Program.cs
+++ HangManGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HangManGame
 {
@@ -11,16 +13,70 @@ namespace HangManGame
 
             while (playAgain)
             {
+                List<string> levelOneWords = new List<string>(); //List of words for level one difficulty              
 
+                levelOneWords.Add("agent");
+                levelOneWords.Add("blood");
+                levelOneWords.Add("chair");
+                levelOneWords.Add("death");
+                levelOneWords.Add("enemy");
+                levelOneWords.Add("gross");
+                levelOneWords.Add("laser");
+                levelOneWords.Add("queen");
+                levelOneWords.Add("smoke");
+                levelOneWords.Add("wheel");
 
+                Random rndLvlOne = new Random();
+                string lvlOneWord = levelOneWords[rndLvlOne.Next(levelOneWords.Count)]; //Randomly selected word from level one list
 
+                int lvlOneAllowedAttempts = 9;
+                int lvlOneWrongAttempts = 0;
+                int lvlOneRemAttempts = 0;
+                int lvlOneRemLetters = 5;
+                char[] lvlOneDsply = {'_','_','_','_','_'};
 
+                Console.WriteLine(lvlOneDsply.ToString());
+
+                Console.WriteLine("Welcome to Hang Man! \n\nLevel One Difficulty \n");
+
+                while (lvlOneWord != lvlOneDsply.ToString() && lvlOneWrongAttempts <= lvlOneAllowedAttempts)  //DANGER: .ToString doesn't do what you might think!
+                {
+                    Console.WriteLine(lvlOneWord);//for test purposes
+
+                    lvlOneRemAttempts = lvlOneAllowedAttempts - lvlOneWrongAttempts;                  
+                    Console.WriteLine(lvlOneDsply);
+                    Console.WriteLine("\nPlease guess a letter\n");
+                    string input = Console.ReadLine();
+                    
+                    if (lvlOneWord.Contains(input))
+                    {
+                        lvlOneRemLetters--;
+                        int letterIndex = lvlOneWord.IndexOf(input);
+                        lvlOneDsply[letterIndex] = input.ToCharArray()[letterIndex]; //Error: selects only first letter all others char[5] can only select one letter if there are two of same 
+                        Console.WriteLine($"Congrats! only {lvlOneRemLetters} left!");
+                    }
+                    else
+                    {
+                        lvlOneWrongAttempts++;
+                        Console.WriteLine($"The word does not contain that letter, you have {lvlOneRemAttempts} chances left.");
+                    }
+
+                }
+
+                if (lvlOneWord == lvlOneDsply.ToString()) //DANGER: .ToString doesn't do what you might think!
+                {
+                    Console.WriteLine("Congratulations! YOU WIN!");
+                }
+                else
+                {
+                    Console.WriteLine("GAME OVER");
+                }
 
 
                 Console.WriteLine("Would you like to play again y/n?");
                 string userAnswer = Console.ReadLine();
              
-                playAgain = userAnswer == yesString; // short option playAgain
+                playAgain = userAnswer == yesString; 
             }
         }
     }
